b578ac7c6fd9edfcbf7b9494fb6853b39edf39b2
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-10 07:25:41 +0800

    Added Route Path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
-
 import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Cart from './pages/Cart/Cart'
+import Collection from './pages/Collection/Collection'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/category/:category' element={<Collection />} />     {/*Dynamic Routing*/}
+        <Route path='/product/:productId' element={<ProductDetails />} />
+      </Routes>
+    </div>
   )
 }
 
 export default App
+
+
