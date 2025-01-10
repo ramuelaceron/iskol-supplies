f4fe5f83f0e1efd795bb892fdbce58d8926e958e
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-10 09:17:31 +0800

    Added Cart Count

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../../context/ShopContext'
+import {MdDelete} from 'react-icons/md'
+import CartTotal from '../../components/CartTotal/CartTotal'
+import './Cart.css'
 
 const Cart = () => {
+
+  const {products, currency, cartItems, updateQuantity} = useContext(ShopContext)
+
+  const [cartData, setCartData] = useState([])
+
+  useEffect(() => {
+    if(products.length === 0) return;
+
+    if(!cartItems || typeof cartItems !== 'object'){
+      setCartData([])
+      return;
+    }
+
+    const tempData = Object.entries(cartItems).flatMap(([itemId, choices]) =>
+    Object.entries(choices).filter(([, quantity]) => quantity > 0).map(([choice, quantity]) => ({
+      _id: itemId,
+      choice,
+      quantity
+    })) 
+    )
+    setCartData(tempData)
+  }, [cartItems, products])
+
   return (
-    <div>Cart</div>
+    <div>
+      <div className="cart-content-container">
+        {
+          cartData.map((item, index) => {
+            const productData = products.find(product => product._id === item._id)
+            return(
+              <div key={index} className='cart-item'>
+                <div className="cart-item-info">
+                  <img src={productData.image[0]} alt="" className='product-cart-image'/>
+                  <div className="product-details-cart">
+                    <p className="cart-product-name">{productData.name}</p>
+                    <div className="product-price-choice">
+                      <p className='cart-product-price'>{currency} {productData.price}.00</p>
+                      <p className="size">{item.choice}</p>
+                    </div>
+                  </div>
+                </div>
+
+                <input type="number" className='quantity-input' min={1} defaultValue={item.quantity}
+                onChange={(e) => e.target.value === '' || e.target.value === 0 ? null : updateQuantity(item._id, item.choice, Number(e.target.value))} />
+              
+                <MdDelete className='delete-icon' onClick={()=> updateQuantity(item._id, item.choice, 0)} />
+              </div>
+            ) 
+          })
+        }
+      </div>
+      <div className="checkout-container">
+        <div className="checkout-box">
+          <CartTotal />
+          <div className="checkout-button-container">
+            <button className="checkout-button">PROCCEED TO CHECKOUT</button>
+          </div>
+        </div>
+      </div>
+      <hr className='footer-divider'/>
+    </div>
   )
 }
 
