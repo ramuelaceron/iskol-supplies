741445c729ab5a542d2293b0242bdf17a939f6a5
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-10 18:06:41 +0800

    Added Controllers

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,27 @@
+
+
+const loginUser = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const registerUser = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const adminLogin = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export {loginUser, registerUser, adminLogin}
