713d3df040621f2470f5e0e2afc36ab9d99dac9e
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-10 16:53:09 +0800

    Added Configuration

---
+++ backend/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=> {
+
+    mongoose.connection.on('connected', ()=> {
+        console.log("Connection available")
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
+
+}
+
+export default connectDB
