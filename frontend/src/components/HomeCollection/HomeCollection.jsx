887b4e80989537f2f8f321ba40c847930fb1a45c
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-10 08:18:58 +0800

    Added Home Products Collection

--- frontend/src/components/HomeCollection/HomeCollection.jsx
+++ frontend/src/components/HomeCollection/HomeCollection.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import {ShopContext} from '../../context/ShopContext'
+import {Link} from 'react-router-dom'
 
 const HomeCollection = () => {
+    const {products} = useContext(ShopContext)
+    const [homeProduct, setHomeProduct] = useState([])
+
+    useEffect(() => {
+        setHomeProduct(products.slice(0, 10))
+    }, [products])
+
   return (
-    <div>HomeCollection</div>
+    <div>
+      <div className="product-container">
+            <div className="list_header">
+                <h1>Our Products</h1>
+                <hr className='divider'/>
+            </div>
+            <div className="product-grid">
+                {homeProduct.length > 0 ? (
+                    homeProduct.map((product) => (
+                        <div className="product-card" key={product._id}>
+                            <div className="product-image">
+                                <Link to={`/product/${product._id}`}>
+                                    <img src={product.image[0]} alt="" />
+                                </Link>
+                            </div>
+                            <h3>{product.name}</h3>
+                            <p>₱ {product.price}.00</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No product is found in this category</p>
+                )}
+            </div>
+        </div>
+        <hr className='footer-divider'/>
+    </div>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import BestSeller from '../../components/LatestCollection/BestSeller'
+import HomeCollection from '../../components/HomeCollection/HomeCollection'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <BestSeller />
+      <HomeCollection />
     </div>
   )
 }
