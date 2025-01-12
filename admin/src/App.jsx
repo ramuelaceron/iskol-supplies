741d311da7ca9f4203bdddaa620c198f395b7c74
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-12 19:56:49 +0800

    Added Admin Panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add/Add"
 import List from "./pages/List/List"
 import Orders from "./pages/Orders/Orders"
+import Login from './components/Login/Login'
 
 const App = () => {
+  const [token, setToken] = useState('')
   return (
     <div className="app-container">
-
-        <div className="app-content">
-          <Sidebar />
-          <div className="page-content">
-            <Routes>
-              <Route path="/add" element={<Add />} />
-              <Route path="/list" element={<List />} />
-              <Route path="/orders" element={<Orders />} />
-            </Routes>
-          </div>
-        </div>
-
+      {
+        token === '' ? (
+          <Login />
+        ) : (
+          <>
+            <div className="app-content">
+              <Sidebar />
+              <div className="page-content">
+                <Routes>
+                  <Route path="/add" element={<Add />} />
+                  <Route path="/list" element={<List />} />
+                  <Route path="/orders" element={<Orders />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+        )
+      }
     </div>
   )
 }
--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import './Login.css'
 
 const Login = () => {
   return (
     <div>
-      Login
+      <div className="admin-panel-container">
+        <div className="admin-panel-box">
+          <h1 className="login-title">Admin Panel</h1>
+          <form>
+            <div className="form-group">
+              <p className="form-label">Email Address</p>
+              <input
+                type="email"
+                className="form-input"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <p className="form-label">Password</p>
+              <input
+                type="paswword"
+                className="form-input"
+                placeholder="Enter password"
+                required
+              />
+            </div>
+            <button className='form-button' type="submit">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
