ebe8efbf4c594e06b42f5d2878dccbed54dded2b
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-12 16:22:44 +0800

    Added Style to Sidebar

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import pupLogo from '../../assets/pupbc.png'
 import {FaCentos} from 'react-icons/fa'
 import {IoIosLogOut, IoMdAddCircleOutline} from 'react-icons/io'
 import {MdFormatListBulletedAdd, MdAddShoppingCart} from 'react-icons/md'
 import {NavLink} from 'react-router-dom'
+import './Sidebar.css'
 
 const Sidebar = () => {
   
   return (
     <div className='sidebar-container'>
       <div className="sidebar-header">
-        <FaCentos className='side-logo' />
+      <img src={pupLogo} alt="PUP Logo" className='side-logo' />
+        <h2>
+            <span className="highlight">ISKO</span>L-Supplies
+        </h2>
       </div>
       <div className="sidebar-links">
         <NavLink className='sidebar-link' to="/add">
