5e258a0a64ad907c92c57d70366dec598a16ab70
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-17 14:50:50 +0800

    changed

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
 connectDB();
