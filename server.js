992ffec04dff0b4ec84e19a60c70bfa57451c924
Ramuel Aceron <aceronramuel@gmail.com>
2025-01-17 12:33:42 +0800

    for deployment

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // api endpoints
-app.use('/api/user', userRouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('/', (req,res)=> {
-    res.send('API WORKING')
-})
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
-app.listen(port, ()=> console.log('Server started on port: ' + port))
+app.listen(port, () => console.log("Server started on port: " + port));
